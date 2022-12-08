22e3a353c04b8b70f1803ac537a8af3200197375
JamalBoustani <boustani11@gmail.com>
2022-12-08 22:04:59 +0000

    Update primes.py

--- primes.py
+++ primes.py
@@ -2,5 +2,19 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if(number_of_primes<1):
+        raise ValueError("0 and negative numbers not accepted")
     list = []
+    count = 0
+    prime_number = 2
+    while count < number_of_primes:
+        for n in range(2,number):
+            if(number % n == 0):
+                break
+        
+        count+=1
+        list.append(number)
+        
+        prime_number += 1
+
     return list
